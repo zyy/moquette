0716a0742539c068976dbc21932b12596b94e27f
zyy <692895299@qq.com>
2015-01-09 11:12:14 +0800

    权限类型文件提交

---
+++ broker/src/main/java/org/eclipse/moquette/server/netty/AuthenticatorType.java
@@ -0,0 +1,10 @@
+package org.eclipse.moquette.server.netty;
+
+/**
+ * Created by yycoder on 2015/1/8.
+ */
+public interface AuthenticatorType {
+    public static final String ALL = "all";
+    public static final String FILE = "file";
+    public static final String DB = "db";
+}
